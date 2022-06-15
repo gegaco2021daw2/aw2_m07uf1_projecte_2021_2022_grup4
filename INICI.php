37945ddd0b473f48bade73de8425b4536802c787
Gerard Gascón <gegaco@inf1-gegaco.fjeclot.net>
2022-06-15 18:27:30 +0200

    projecte-m07uf1-grup4

---
+++ INICI.php
@@ -0,0 +1,45 @@
+<?php
+    $arch="";
+    $arch = fopen ("comandes", "w+");
+    fwrite($arch, "");
+    fclose($arch);
+?>
+    <html>
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            
+            <TITLE>Projecte M07</TITLE>
+    </head>
+
+    <body>
+        <div class="back"></div>
+        <div class="INICI">
+            <div class="cuadreinici">
+                <div class="cuadreinici_1">
+                  
+                </div>
+                <br><br><br><br>
+                <div class="cuadreinici_2">
+                    <a href="INICIUSER.html">
+                        
+                        <p class="pinici">USUARI</p>
+                    </a>
+                </div>
+                <div class="cuadreinici_3">
+                    <a href="INICIBIBLIOTECARI.html">
+                        
+                        <p class="pinici">BIBLIOTECARI</p>
+                    </a>
+                </div>
+                <div class="cuadreinici_4">
+                    <a href="INICICAP.html">
+                        
+                        <p class="pinici">CAP</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </body>
+
+    </html>
